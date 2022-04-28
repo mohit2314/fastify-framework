f8168e2276ac20549cf981969a052758afdf9380
Mohit2314 <mohitpatel2314@gmail.com>
2022-04-28 18:22:19 +0530

    added post request

--- controllers/itemsController.js
+++ controllers/itemsController.js
@@ -1,4 +1,5 @@
-const items= require('../items')
+const {v4:uuidv4} = require('uuid')
+let items= require('../items')
 
 
 const getItems= (req,reply) =>{
@@ -11,6 +12,19 @@ const getItem =(req,reply) =>{
     reply.send(item)
 }
 
+const addItem = (req,reply) =>{
+
+    const {name} = req.body;
+    const item={
+        id:uuidv4(),
+        name
+    }
+
+    items=[...items,item]
+
+    reply.code(201).send(item)
+}
+
 module.exports={
-    getItems,getItem
+    getItems,getItem,addItem
 }
