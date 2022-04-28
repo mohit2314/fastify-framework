5ff4e3bb367d5b43424568e2c1158b51f31f1aff
Mohit2314 <mohitpatel2314@gmail.com>
2022-04-28 17:54:28 +0530

    separated handler function

--- routes/items.js
+++ routes/items.js
@@ -1,19 +1,50 @@
 const items = require('../items');
 
+// Item Schema
+const Item= {
+    type:'object',
+    properties:{
+        id:{type:'string'},
+        name:{type:'string'}
+    }
+}
+
+//OPtions for get all items
+const getItemsOpts={
+    schema:{
+        response:{
+            200:{
+                type:'array',
+                items:Item
+            }
+        }
+    },
+    handler:function (req,reply){
+        reply.send(items)
+    }
+}
+
+const getItemOpts={
+    schema:{
+        response:{
+            200:Item
+        }
+    },
+    handler:function (req,reply) {
+
+        const {id} = req.params;
+        const item = items.find((item)=> item.id ===id)
+        reply.send(item)
+    }
+}
+
 function itemRoutes(fastify,options,done){
 
-fastify.get('/items',(req,reply) =>{
-    reply.send(items)
-})
+fastify.get('/items',getItemsOpts)
 
 
 //return single item
-fastify.get('/items/:id',(req,reply) =>{
-
-    const {id} = req.params;
-    const item = items.find((item)=> item.id ===id)
-    reply.send(item)
-})
+fastify.get('/items/:id', getItemOpts)
 
 done()
 }
